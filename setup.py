d0ebcd5fe96b5b9f255eff43bca977b38eef541f
yankuic <yankuic@gmail.com>
2023-12-13 12:13:59 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -2,17 +2,11 @@ from setuptools import setup
 
 setup(
     name='gdstools',
-    py_modules=[],
+    version='0.1',
+    # py_modules=[],
+    packages=['gdstools'],
     install_requires=[
-        'numpy',
-        'matplotlib',
-        'tqdm',
         'python-dotenv',
         'earthengine-api>=0.1.35',
-        'geetools',
-        'rasterio',
-        'pandas',
-        'seaborn',
-        'rio-cogeo'
     ]
 )
